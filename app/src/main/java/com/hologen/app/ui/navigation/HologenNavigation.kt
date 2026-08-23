b48163ee2c3e29ff086ba662b4064084350a84bb
Prvin <anewproject229@gmail.com>
2026-08-23 11:17:14 +0530

    Update HologenNavigation.kt

--- app/src/main/java/com/hologen/app/ui/navigation/HologenNavigation.kt
+++ app/src/main/java/com/hologen/app/ui/navigation/HologenNavigation.kt
@@ -1,7 +1,7 @@
+
 package com.hologen.app.ui.navigation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clip // <-- YE IMPORT SABSE ZARURI HAI
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -27,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip // <-- YE SAHI IMPORT HAI (ui.draw.clip)
 import com.hologen.app.ui.theme.HologenColors
 import com.hologen.app.ui.theme.HologenMetrics
 
